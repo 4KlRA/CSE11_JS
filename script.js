184af9b5007c4c809baaa6f29aa6f12f8b5cdfbb
123-AKIRA <1akiraforp@gmail.com>
2026-02-27 12:19:31 +0530

    commit 4

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //Lec 1 20 02 2026
 console.log("Hello World!");
-document.write("This is a test of document.write()");
+document.write("This is a test of document.write()"+"<br>");
 // single line comment
 /*Mutli line comment*/
 
@@ -54,9 +54,55 @@ if (sync1 === sync2) {
 }
 
 var count;
-document.write{"Starting Loop"+"<br>"};
-for(count = 0, count<10, count++) {
+document.write("Starting Loop"+"<br>");
+for (count = 0; count<10; count++) {
     document.write("Current Count:", count);
     document.write("<br>");
 }
-document.write("Loop Stopped!");
+document.write("Loop Stopped!"+"<br>");
+
+//Lec 4 27 02 2026
+var count = 0;
+document.write("Starting Loop"+"<br>");
+while (count < 10) {
+    document.write("Current Count: " + count + "<br>");
+    count++;
+}
+document.write("Loop Stopped!"+"<br>");
+
+let age = 20;
+if (age > 18) {
+    document.write("You are an adult!"+"<br>");
+} else {    
+    document.write("You are a minor!"+"<br>");
+}
+
+var grade = 'A';
+document.write("Entering Switch Statement"+"<br>");
+switch (grade) {
+    case 'A':
+        document.write("Excellent!"+"<br>");
+        break;
+    case 'B':
+        document.write("Good!"+"<br>");
+        break;
+    case 'C':
+        document.write("Average!"+"<br>");
+        break;
+    case 'D':
+        document.write("Below Average!"+"<br>");
+        break;
+    case 'F':
+        document.write("Failed!"+"<br>");
+        break;
+    default:
+        document.write("Invalid Grade!"+"<br>");
+}
+//without parameter function
+function  myFunction() {
+    alert("Hello World!");
+}
+//with parameter function
+function SayHello(name,age) {
+    document.write(name + " is " + age + " years old!"+"<br>");
+}
